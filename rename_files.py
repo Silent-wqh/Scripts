8eecc790dfebb6c4c61f38b00b3297f6be182dac
Silent-wqh <wqh1216dyx@qq.com>
2023-07-16 11:27:47 +0800

    rename_files.py 修复了重复添加日期的bug，并且只要文件末尾添加了日期无论这个日期是否与文件的最后修改日期是否相同均不会再添加日期。

--- rename_files.py
+++ rename_files.py
@@ -1,13 +1,14 @@
 """
 rename_files.py
 
-这个脚本用于重命名指定目录中的所有文件。它将文件的扩展名更改为指定的前缀，并将其添加到文件名的开头，以形成"新前缀.原始文件名.原始扩展名"的格式。
+这个脚本用于重命名指定目录中的所有文件。它将文件的扩展名更改为指定的前缀，并将其添加到文件名的开头，以形成"新前缀.原始文件名.日期.原始扩展名"的格式。
 
 用法：
-    python rename_files.py /path/to/your/directory
+    python rename_files.py /path/to/your/directory [--add_date]
 
 注意：
     你需要将 "/path/to/your/directory" 替换为你的目标文件夹的实际路径。
+    使用 --add_date 参数可以将文件的最后修改日期添加到文件名中，如果文件名已经包含了日期，则不会重复添加。
 
 规则：
     匹配：^xlsx?(.*?)$ 替换：Excel\\1
@@ -27,8 +28,9 @@ rename_files.py
 import os
 import argparse
 import re
+from datetime import datetime
 
-def rename_files_in_directory(target_dir):
+def rename_files_in_directory(target_dir, add_date):
     # 定义重命名规则
     rules = [
         (r'^xlsx?(.*?)$', 'Excel\\1'),
@@ -60,13 +62,25 @@ def rename_files_in_directory(target_dir):
                     new_ext = re.sub(pattern, replacement, ext)
                     # 只有在基础名不以新前缀开头时才添加新前缀
                     if not base_name.startswith(new_ext):
-                        new_filename = f"{new_ext}.{base_name}.{ext}"
+                        new_base_name = f"{new_ext}.{base_name}"
                     else:
-                        new_filename = filename
+                        new_base_name = base_name
                     break
             else:
                 # 如果没有任何规则匹配，保持原样
-                new_filename = filename
+                new_base_name = base_name
+
+            # 如果需要添加日期，获取文件的最后修改时间并添加到文件名
+            if add_date:
+                mtime = os.path.getmtime(os.path.join(target_dir, filename))
+                date = datetime.fromtimestamp(mtime).strftime('%y-%m-%d')
+                # 只有在基础名不以日期格式结尾时才添加新日期
+                if not re.search(r'\d{2}-\d{2}-\d{2}$', new_base_name):
+                    new_filename = f"{new_base_name}.{date}.{ext}"
+                else:
+                    new_filename = f"{new_base_name}.{ext}"
+            else:
+                new_filename = f"{new_base_name}.{ext}"
 
             # 获取文件的原始路径和新路径
             old_file_path = os.path.join(target_dir, filename)
@@ -79,8 +93,9 @@ def rename_files_in_directory(target_dir):
 parser = argparse.ArgumentParser(description='Rename files in a directory.')
 # 添加一个命令行参数
 parser.add_argument('directory', help='The directory where files will be renamed.')
+parser.add_argument('--add_date', action='store_true', help='Add the last modified date to the filename.')
 # 解析命令行参数
 args = parser.parse_args()
 
 # 调用函数
-rename_files_in_directory(args.directory)
+rename_files_in_directory(args.directory, args.add_date)
