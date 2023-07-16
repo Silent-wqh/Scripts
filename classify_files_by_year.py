82254fc6bd75dc7b76b22266dab6f70cdd39a6ed
Silent-wqh <wqh1216dyx@qq.com>
2023-07-16 20:12:57 +0800

    classify_files_by_year.py增加了配置文件功能，可以通过配置文件指定需要处理的文件夹

--- classify_files_by_year.py
+++ classify_files_by_year.py
@@ -1,36 +1,53 @@
 import os
 import shutil
 import argparse
+import yaml
 from datetime import datetime
 
-def classify_files_by_year(target_dir, keep_current_year_files_in_root=True):
-    # 获取目标文件夹下的所有文件
-    files = os.listdir(target_dir)
-    current_year = datetime.now().year
-    for file_name in files:
-        # 获取文件的全路径
-        file_path = os.path.join(target_dir, file_name)
-        # 获取文件的创建时间和修改时间，并选择其中较早的一个，然后格式化为年份
-        create_time = os.path.getctime(file_path)
-        modify_time = os.path.getmtime(file_path)
-        file_year = datetime.fromtimestamp(min(create_time, modify_time)).year
-        # 如果启用了keep_current_year_files_in_root选项，并且文件的年份是当前年份，那么直接跳过
-        if keep_current_year_files_in_root and file_year == current_year:
-            continue
-        # 创建对应年份的文件夹，如果文件夹已经存在则不会创建
-        os.makedirs(os.path.join(target_dir, str(file_year)), exist_ok=True)
-        # 移动文件到对应的年份文件夹
-        shutil.move(file_path, os.path.join(target_dir, str(file_year), file_name))
+def classify_files_and_folders_by_year(target_dirs, keep_current_year_files_in_root=True):
+    for target_dir in target_dirs:
+        # 获取目标文件夹下的所有文件和文件夹
+        items = os.listdir(target_dir)
+        current_year = datetime.now().year
+        for item_name in items:
+            # 获取项目的全路径
+            item_path = os.path.join(target_dir, item_name)
+            # 如果项目是文件夹，并且它的名字是四位数（例如"2021"或"2022"），那么直接跳过
+            if os.path.isdir(item_path) and item_name.isdigit() and len(item_name) == 4:
+                continue
+            # 获取项目的创建时间和修改时间，并选择其中较早的一个，然后格式化为年份
+            create_time = os.path.getctime(item_path)
+            modify_time = os.path.getmtime(item_path)
+            item_year = datetime.fromtimestamp(min(create_time, modify_time)).year
+            # 如果启用了keep_current_year_files_in_root选项，并且项目的年份是当前年份，那么直接跳过
+            if keep_current_year_files_in_root and item_year == current_year:
+                continue
+            # 创建对应年份的文件夹，如果文件夹已经存在则不会创建
+            os.makedirs(os.path.join(target_dir, str(item_year)), exist_ok=True)
+            # 移动项目到对应的年份文件夹
+            shutil.move(item_path, os.path.join(target_dir, str(item_year), item_name))
+
+def load_config(config_path):
+    with open(config_path, 'r', encoding="utf8") as f:
+        return yaml.safe_load(f)
 
 if __name__ == "__main__":
     # 创建一个命令行参数解析器
-    parser = argparse.ArgumentParser(description='Classify files by year.')
-    parser.add_argument('target_dir', help='The target directory.')
+    parser = argparse.ArgumentParser(description='Classify files and folders by year.')
+    parser.add_argument('-t', '--target-dirs', nargs='*', help='The target directories.')
+    parser.add_argument('-c', '--config-path', default='./config/classify_files_by_year_config.yaml', help='The path of the configuration file.')
     parser.add_argument('--no-keep-current-year-files-in-root', action='store_true',
-                        help='If specified, do not keep the files of the current year in the root directory.')
+                        help='If specified, do not keep the items of the current year in the root directory.')
 
     # 解析命令行参数
     args = parser.parse_args()
 
-    # 对目标文件夹下的文件进行分类
-    classify_files_by_year(args.target_dir, not args.no_keep_current_year_files_in_root)
+    if args.target_dirs is None:
+        # 如果没有在命令行参数中指定目标文件夹，那么从配置文件中读取
+        config = load_config(args.config_path)
+        args.target_dirs = config['target_dirs']
+        if 'no_keep_current_year_files_in_root' in config:
+            args.no_keep_current_year_files_in_root = config['no_keep_current_year_files_in_root']
+
+    # 对目标文件夹下的文件和文件夹进行分类
+    classify_files_and_folders_by_year(args.target_dirs, not args.no_keep_current_year_files_in_root)
